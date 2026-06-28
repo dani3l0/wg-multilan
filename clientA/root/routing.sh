bbd8db1f38bf186028a40823ea8f189afcfe1fc0
dani3l0 <daniel.jank@protonmail.com>
2026-06-28 17:09:40 +0200

    Better routing.sh

--- clientA/root/routing.sh
+++ clientA/root/routing.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
-iptables -A FORWARD -i wg0 -o wlan0 -j ACCEPT
-iptables -A FORWARD -i wlan0 -o wg0 -j ACCEPT
-iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
-iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
+
+# FORWARD Rulez
+if ! iptables -C FORWARD -i wg0 -o wlan0 -j ACCEPT 2>/dev/null; then
+	iptables -A FORWARD -i wg0 -o wlan0 -j ACCEPT
+fi
+if ! iptables -C FORWARD -i wlan0 -o wg0 -j ACCEPT 2>/dev/null; then
+    iptables -A FORWARD -i wlan0 -o wg0 -j ACCEPT
+fi
+
+# NAT MASQUERADE Rulez
+if ! iptables -t nat -C POSTROUTING -o wlan0 -j MASQUERADE 2>/dev/null; then
+    iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+fi
+if ! iptables -t nat -C POSTROUTING -o wg0 -j MASQUERADE 2>/dev/null; then
+    iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
+fi
+
 exit 0
